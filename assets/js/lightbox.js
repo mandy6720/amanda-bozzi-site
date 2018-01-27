5888817ed1b1c99231aecbd3242eea9dc368e4f1
mandy6720 <amanda.hewitt01@gmail.com>
2018-01-27 14:07:25 -0500

    Remove auto capitalize on text inputs and changes lightboxes to use html

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -4,7 +4,7 @@ var abc = $("#abc").click(function(el) {
 });
 var abcLb = basicLightbox.create(`
   <div class="modal1">
-    <img src="assets/images/portfolio/ab-coaching.png" width="800" height="600">
+    <img src="assets/images/portfolio/ab-coaching.png" width="200" height="100">
       <a>Close</a>
   </div>
 `, {
@@ -16,12 +16,11 @@ var abcLb = basicLightbox.create(`
 
 // Releaf lightbox
 var releaf = $("#releaf").click(function(el) {
-  console.log('releaf')
   releafLb.show();
 });
 var releafLb = basicLightbox.create(`
   <div class="modal1">
-    <img src="assets/images/portfolio/releaf-feels.png" width="800" height="600">
+    <img src="assets/images/portfolio/releaf-feels.png" width="200" height="100">
     <a>Close</a>
   </div>
 `, {
@@ -35,14 +34,27 @@ var inprnt = $("#inprnt").click(function(el) {
   inprntLb.show();
 });
 var inprntLb = basicLightbox.create(`
-    <img src="assets/images/portfolio/inprnt-discovery.png" width="800" height="600">
-`);
+  <div class="modal1">
+    <img src="assets/images/portfolio/inprnt-discovery.png" width="200" height="100">
+    <a>Close</a>
+  </div>
+`, {
+  beforeShow: function (instance) {
+    instance.element().querySelector('a').onclick = instance.close
+  }
+});
 
 //  IBM lightbox
 var ibm = $("#ibm").click(function(el) {
-  console.log('ibm')
   ibmLb.show();
 });
 var ibmLb = basicLightbox.create(`
-    <img src="assets/images/portfolio/level2.png" width="800" height="600">
-`);
+  <div class="modal1">
+    <img src="assets/images/portfolio/level2.png" width="200" height="100">
+    <a>Close</a>
+  </div>
+`, {
+  beforeShow: function (instance) {
+    instance.element().querySelector('a').onclick = instance.close
+  }
+});
